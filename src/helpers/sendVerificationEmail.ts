9e9af6ab914b7a872f83823f40cf6d4862eb905f
ankitmahotla <ankitmahotla45@gmail.com>
2024-08-19 12:33:12 +0530

    verify code and username validation

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
     try {
         await resend.emails.send({
-            from: 'onboarding@whispr.dev',
+            from: 'onboarding@resend.dev',
             to: email,
             subject: 'Mystery Message | Verification code',
             react: VerificationEmail({username, otp: verifyCode}),
